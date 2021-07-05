aefa88e37192554315686b711a8e667abda0ab61
Matheus Calixto <matheuscalixto8@gmail.com>
2021-07-05 19:20:31 -0300

    Update - Home e Login

--- app/src/main/java/com/pdm/recycle/MainActivity.java
+++ app/src/main/java/com/pdm/recycle/MainActivity.java
@@ -2,13 +2,21 @@ package com.pdm.recycle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+    }
+
+    public void redirectLogin(View v) {
+        Intent intent = new Intent(this, SignInActivity.class);
+        startActivity(intent);
     }
 }
