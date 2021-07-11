ea8a3e27d2a0052c7cba31a2a01f56db0ebce09c
Matheus Calixto <matheuscalixto8@gmail.com>
2021-07-11 00:10:44 -0300

    Implementacao - Cadastro

--- app/src/main/java/com/pdm/recycle/MainActivity.java
+++ app/src/main/java/com/pdm/recycle/MainActivity.java
@@ -19,4 +19,9 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, SignInActivity.class);
         startActivity(intent);
     }
+
+    public void redirectRegister(View v) {
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
+    }
 }
--- app/src/main/java/com/pdm/recycle/SignInActivity.java
+++ app/src/main/java/com/pdm/recycle/SignInActivity.java
@@ -9,6 +9,7 @@ public class SignInActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_sign_in);
     }
 }
