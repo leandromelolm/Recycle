4f49b90302f79c997be765f8273758ae3e69924e
Leandro Melo <lsm@a.recife.ifpe.edu.br>
2021-08-01 17:12:03 -0300

    SignIn - Login autenticação com firebase.

--- app/src/main/java/com/pdm/recycle/view/SignInActivity.java
+++ app/src/main/java/com/pdm/recycle/view/SignInActivity.java
@@ -1,24 +1,110 @@
 package com.pdm.recycle.view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.firebase.auth.FirebaseUser;
 import com.pdm.recycle.R;
+import com.pdm.recycle.control.ConfiguracaoFirebase;
+import com.pdm.recycle.model.Usuario;
 
 public class SignInActivity extends AppCompatActivity {
+
+    private EditText campoEmail, campoSenha;
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_sign_in);
+
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+
+        campoEmail = findViewById(R.id.LoginEmail);
+        campoSenha = findViewById(R.id.LoginPassword);
+
     }
 
-    public void redirecHome(View v) {
+    public void logarUsuario(Usuario usuario){
+
+        autenticacao.signInWithEmailAndPassword(
+                usuario.getEmail(), usuario.getSenha()
+        ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if( task.isSuccessful() ){
+                    abrirTelaPrincipal();
+                }else {
+
+                    String excecao = "";
+                    try {
+                        throw task.getException();
+                    }catch ( FirebaseAuthInvalidUserException e ) {
+                        excecao = "Usuário não está cadastrado.";
+                    }catch ( FirebaseAuthInvalidCredentialsException e ){
+                        excecao = "E-mail e senha não correspondem a um usuário cadastrado";
+                    }catch (Exception e){
+                        excecao = "Erro ao cadastrar usuário: "  + e.getMessage();
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(SignInActivity.this,
+                            excecao,
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    public void validarAutenticacaoUsuario(View view){
+
+        //Recuperar textos dos campos
+        String textoEmail = campoEmail.getText().toString();
+        String textoSenha = campoSenha.getText().toString();
+
+        //Validar se e-mail e senha foram digitados
+        if( !textoEmail.isEmpty() ){
+            if( !textoSenha.isEmpty() ){
+
+                Usuario usuario = new Usuario();
+                usuario.setEmail( textoEmail );
+                usuario.setSenha( textoSenha );
+
+                logarUsuario( usuario );
+
+            }else {
+                Toast.makeText(SignInActivity.this,
+                        "Preencha a senha!",
+                        Toast.LENGTH_SHORT).show();
+            }
+        }else {
+            Toast.makeText(SignInActivity.this,
+                    "Preencha o email!",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+    
+    public void abrirTelaPrincipal(){
+        Intent intent = new Intent(SignInActivity.this, HomeActivity.class);
+        startActivity( intent );
+    }
+
+    /*public void redirecHome(View v) {
         Intent intent = new Intent(this, HomeActivity.class);
         startActivity(intent);
-    }
+    }*/
 }
